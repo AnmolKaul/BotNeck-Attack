9f1d8a76c1799d4a319b4d4492ac8e807d7c1e10
Anmol Kaul <ezioaltair1234@gmail.com>
2021-03-31 11:58:21 +0530

    init

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+    [SerializeField] private Transform serverPosition;
+
+    private LineRenderer lineRenderer;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, serverPosition.position);
+
+        Mesh lineMesh = new Mesh();
+        lineRenderer.BakeMesh(lineMesh, Camera.main, true);
+
+        MeshCollider collider = transform.gameObject.AddComponent<MeshCollider>();
+    }
+
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log(hit.transform.name);
+        }
+
+    }
+}
